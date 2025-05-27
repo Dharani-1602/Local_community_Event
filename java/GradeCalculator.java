6ee6bd1e7f5365520b816a1b55478fc23055558c
Dharani-1602 <229x1a0579@gprec.ac.in>
2025-05-27 12:32:30 +0530

    Create GradeCalculator.java

---
+++ java/GradeCalculator.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class GradeCalculator
+  {
+    public static void main(String args[])
+    {
+      Scanner s=new Scanner(System.in);
+      System.out.print("Enter the marks out of 100: ");
+      int marks=s.nextInt();
+      if(marks>=90)
+      {
+        System.out.println("Grade A");
+      }
+      else if(marks>=80)
+      {
+        System.out.println("Grade B");
+      }
+      else if(marks>=70)
+      {
+        System.out.println("Grade C");
+      }
+      else if(marks>=60)
+      {
+        System.out.println("Grade D");
+      }
+      else
+      {
+        System.out.println("Grade F");
+      }
+    }
+  }
